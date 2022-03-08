76d2633fe565b90c3198488c87575f351f58a14d
Micheal Scott Evans <michealsevans20@gmail.com>
2022-03-08 15:48:21 -0600

    Biography, Education, Job Exp, Goals have been added. app.js has been added as well

---
+++ app.js
@@ -0,0 +1,53 @@
+let answerOne = prompt('Are you over 18?').toUpperCase();
+
+
+if(answerOne === 'Y' || answerOne === 'YES'){
+  alert('You are correct!');
+} else if(answerOne === 'N' || answerOne === 'NO'){
+  alert('Sorry, you are incorrect');
+}
+
+
+
+
+let answerTwo = prompt('Do you live in a state where is mostly hot?').toUpperCase();
+
+
+if(answerTwo === 'Y' || answerTwo === 'YES'){
+  alert('You are correct!');
+} else if(answerTwo === 'N' || answerTwo === 'NO'){
+  alert('Sorry, you are incorrect');
+}
+
+
+let answerThree = prompt('Should you get plenty of greens to stay healthy?').toUpperCase();
+
+
+if(answerThree === 'Y' || answerThree === 'YES'){
+  alert('You are correct!');
+} else if(answerThree === 'N' || answerThree === 'NO'){
+  alert('Sorry, you are incorrect');
+}
+
+
+
+let answerFour = prompt('Do you have any siblings?').toUpperCase();
+
+
+if(answerFour === 'Y' || answerFour === 'YES'){
+  alert('You are correct!');
+} else if(answerFour === 'N' || answerFour === 'NO'){
+  alert('Sorry, you are incorrect');
+}
+
+
+
+
+let answerFive = prompt('Do you like swimming?').toUpperCase();
+
+
+if(answerFive === 'Y' || answerFive === 'YES'){
+  alert('You are correct!');
+} else if(answerFive === 'N' || answerFive === 'NO'){
+  alert('Sorry, you are incorrect');
+}
